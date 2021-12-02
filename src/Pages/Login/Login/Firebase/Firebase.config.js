7e73f3508855d0a056dcb8befeae4c6a8be59f95
tanveer-610 <tanvirul35-2779@diu.edu.bd>
2021-12-03 00:24:16 +0600

    update

--- src/Pages/Login/Login/Firebase/Firebase.config.js
+++ src/Pages/Login/Login/Firebase/Firebase.config.js
@@ -1,9 +1,4 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCV9Qy2QEtTJnDGvrqFv53a3-vtgmx3Q24",
-    authDomain: "car-house-e46f8.firebaseapp.com",
-    projectId: "car-house-e46f8",
-    storageBucket: "car-house-e46f8.appspot.com",
-    messagingSenderId: "940647820110",
-    appId: "1:940647820110:web:8789fdaaea2fae5a5b4c1f"
+    /*write your token*/
 };
 export default firebaseConfig;
